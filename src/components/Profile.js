2c33bcb2aa3c223239bb6524c180e6508fc43152
Cian Galligan <ciangalligan7@gmail.com>
2021-03-19 00:24:48 +0000

    Banner header and profile pic update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,23 +4,17 @@ import { UserContext } from "../providers/UserProvider";
 
 const Profile = () => {
   const user = useContext(UserContext);
-  const { photoURL, displayName, email } = user;
+  const {photoURL, displayName, email } = user;
   return (
-    <div class="row gutters-sm">
-    <div class="col-md-4 mb-3">
-      <div class="card">
-        <div class="card-body">
-          <div class="d-flex flex-column align-items-center text-center">
-          <img src={photoURL} alt="na" />
-            <div class="mt-3">
-              <h4>{displayName}</h4>
-              <h5>{email}</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-      </div>
+    
+<div class="header">
+    <img src= {photoURL} alt="my image" class="floatdown"/>
+    this is my header, image could go here too
+</div>
+
+
+
+
   );
 };
 
