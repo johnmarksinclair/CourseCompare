d5728d893b319bb8b883cc0a04b3cb1890332009
Cian Galligan <ciangalligan7@gmail.com>
2021-03-21 14:53:39 +0000

    test for vercel deployment

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,36 @@
 import { useContext } from "react";
 import { UserContext } from "../providers/UserProvider";
+import Card from "react-bootstrap/Card";
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button'
+
 //mport { auth } from "../firebase";
 
 const Profile = () => {
   const user = useContext(UserContext);
   const { photoURL, displayName, email } = user;
   return (
-    <div>
-      <img src={photoURL} alt="na" />
-      <h2>{displayName}</h2>
-      <h2>{email}</h2>
-    </div>
-  );
+<div>
+<div class="header">
+    <img src= {photoURL} alt="my image" class="floatdown"/>
+</div>
+
+<div class = "body">
+<Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title class ="text-3xl font-bold pb-2.5	" >{displayName}</Card.Title>
+    <Card.Text class= "pb-1.5">
+      {email}
+    </Card.Text>
+    <Button variant="primary">Share Profile</Button>
+  </Card.Body>
+</Card>
+<div class=" py-10 block rounded-md focus:underline">
+        <h3 class="px-2">My Reviews</h3>
+      </div>
+      </div>
+      </div>
+ );
 };
 
 export default Profile;
