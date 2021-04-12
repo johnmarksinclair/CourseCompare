4f6468cac63c8948627989a56b92d39e7352412c
DesLeeHL <lih4@tcd.ie>
2021-04-12 16:37:09 +0100

    front end module page

--- src/components/Module.js
+++ src/components/Module.js
@@ -17,11 +17,20 @@ const Module = ({ match }) => {
   };
 
   return (
-    <div>
-      <div>{moduleData.id}</div>
-      <div>{moduleData.title}</div>
-      <div>{moduleData.lecturer}</div>
-      <div>{moduleData.description}</div>
+    <div className="p-4">
+      <div className="bg-white shadow-md rounded-xl overflow-hidden">
+        <div className="p-2 bg-blue-500 hover:bg-blue-400 border-b shadow-inner">
+          <div className="py-2 text-white text-5xl text-center font-bold">{moduleData.title}</div>
+        </div>
+        <div className="py-2 border-b text-center">
+          <div className="text-xl text-gray-500 font-thin">Lecturer</div>
+          <div className="text-3xl text-black font-semibold"><span className="border-t">{moduleData.lecturer}</span></div>
+        </div>
+        <div className="px-4 py-2">
+          <div className="text-xl text-gray-500 text-center font-thin"><span className="border-b">Module Description</span></div>
+          <div className="mb-2">{moduleData.description}</div>
+        </div>
+      </div>
     </div>
   );
 };
